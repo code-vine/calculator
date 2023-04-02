a8231ed78b02d0c684417d30b36e8c6adccd3a09
code-vine <caldwell.vincent@outlook.com>
2023-04-02 13:32:10 -0500

    adds markup to Calculator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Calculator from './Calculator';
 
 class App extends React.Component{
   render(){
     return (
       <div className="App">
-        
+        <Calculator />
       </div>
     );
   }
---
+++ src/Calculator.js
@@ -0,0 +1,40 @@
+import './Calculator.css'
+import React from 'react'
+
+class Calculator extends React.Component{
+    render(){
+        return (
+            <div className='calculator-div'>
+                <div id="display">
+                    <p className='display-calculation'>1/2+3</p>
+                    <p className='display-current'>0</p>
+                </div>
+                <div className='keypad'>
+                    <div className='operators'>
+                        <div id="clear">C</div>
+                        <div id="divide">/</div>
+                        <div id="multiply">*</div>
+                        <div id="subtract">-</div>
+                        <div id="add">+</div>
+                        <div id="equals">=</div>
+                    </div>
+                    <div className='numbers'>
+                        <div id="nine">9</div>
+                        <div id="eight">8</div>
+                        <div id="seven">7</div>
+                        <div id="six">6</div>
+                        <div id="five">5</div>
+                        <div id="four">4</div>
+                        <div id="three">3</div>
+                        <div id="two">2</div>
+                        <div id="one">1</div>
+                        <div id="zero">0</div>
+                    </div>
+                    
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Calculator;
